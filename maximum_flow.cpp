3e1ceba8166c69194700515c51cebd6eabdf96c7
nysta12 <nystromvictor@gmail.com>
2025-07-07 13:15:41 +0200

    Min_cut and max_flow completed

--- maximum_flow.cpp
+++ maximum_flow.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <sstream>
 #include <climits>
 using namespace std;
 
@@ -9,7 +8,7 @@ const int NO_PARENT = -2;
 
 int n, m, s, t;
 
-int bfs(vector<vector<int>>& flow_capacity, vector<vector<int>>& adjacent, vector<int>& parent){
+int bfs(vector<vector<int>>& flow_capacity, vector<vector<int>>& adjacent, vector<int>& parent) {
     fill(parent.begin(), parent.end(), -1);
     parent[s] = NO_PARENT;
     queue<pair<int, int>> q;
@@ -19,14 +18,12 @@ int bfs(vector<vector<int>>& flow_capacity, vector<vector<int>>& adjacent, vecto
         int current = q.front().first;
         int flow = q.front().second;
         q.pop();
-
         for (int next_node : adjacent[current]){
             if (parent[next_node] == -1 && flow_capacity[current][next_node] > 0){
                 parent[next_node] = current;
                 int new_flow = min(flow, flow_capacity[current][next_node]);
-                if (next_node == t){
+                if (next_node == t)
                     return new_flow;
-                }
                 q.push({next_node, new_flow});
             }
         }
@@ -34,9 +31,9 @@ int bfs(vector<vector<int>>& flow_capacity, vector<vector<int>>& adjacent, vecto
     return 0;
 }
 
-int maxflow(vector<vector<int>>& flow_capacity, vector<vector<int>>& adjacent){
+int maxflow(vector<vector<int>>& flow_capacity, vector<vector<int>>& adjacent, vector<vector<int>>& flow_result){
     int flow = 0;
-    vector<int> parent(n, -1);
+    vector<int> parent(n);
     int new_flow;
 
     while ((new_flow = bfs(flow_capacity, adjacent, parent))){
@@ -46,48 +43,42 @@ int maxflow(vector<vector<int>>& flow_capacity, vector<vector<int>>& adjacent){
             int previous = parent[current];
             flow_capacity[previous][current] -= new_flow;
             flow_capacity[current][previous] += new_flow;
+            flow_result[previous][current] += new_flow;
+            flow_result[current][previous] -= new_flow;
             current = previous;
         }
     }
     return flow;
 }
 
-void minimum_cut(int u, vector<vector<int>>& flow_capacity, vector<vector<int>>& adjacent, vector<bool>& visited){
-    visited[u] = true;
-    for (int v : adjacent[u]){
-        if (!visited[v] && flow_capacity[u][v] > 0){
-            minimum_cut(v, flow_capacity, adjacent, visited);
-        }
-    }
-}
-
-int main(){
+int main() {
     cin >> n >> m >> s >> t;
     vector<vector<int>> flow_capacity(n, vector<int>(n, 0));
+    vector<vector<int>> flow_result(n, vector<int>(n, 0));
     vector<vector<int>> adjacent(n);
+    vector<pair<int,int>> input_edges;
 
-    for (int i = 0; i < m; ++i){
-        int u, v, w;
-        cin >> u >> v >> w;
+    for (int i = 0; i < m; ++i) {
+        int u, v, c;
+        cin >> u >> v >> c;
         adjacent[u].push_back(v);
         adjacent[v].push_back(u);
-        flow_capacity[u][v] += w;
+        flow_capacity[u][v] += c;
+        input_edges.push_back({u, v});
     }
 
-    maxflow(flow_capacity, adjacent);
-    vector<bool> visited(n, false);
-    minimum_cut(s, flow_capacity, adjacent, visited);
+    int total_flow = maxflow(flow_capacity, adjacent, flow_result);
 
-    vector<int> U;
-    for (int i = 0; i < n; ++i){
-        if (visited[i]){
-            U.push_back(i);
+    vector<tuple<int,int,int>> result_edges;
+    for (auto [u, v] : input_edges) {
+        if (flow_result[u][v] > 0) {
+            result_edges.emplace_back(u, v, flow_result[u][v]);
         }
     }
 
-    cout << U.size() << endl;
-    for (int k : U){
-        cout << k << endl;
+    cout << n << " " << total_flow << " " << result_edges.size() << "\n";
+    for (auto [u, v, f] : result_edges) {
+        cout << u << " " << v << " " << f << "\n";
     }
 
     return 0;
